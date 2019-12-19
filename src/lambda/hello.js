248b5a909a508f37586ffe8f35db95aa41559eb2
Justin Hardin <jpmitchell293@gmail.com>
2019-12-19 14:51:22 -0800

    oppt

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,77 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
+const optimizelySDK = require('@optimizely/optimizely-sdk'),
+  defaultLogger = require('@optimizely/optimizely-sdk/lib/plugins/logger'),
+  rp = require('request-promise');
+
+let optimizelyClientInstance;
+let initOptimizely = async (projectID) => {
+
+  // Options to get the datafile via CDN
+  let options = {
+    uri: `https://cdn.optimizely.com/json/17147660659.json`,
+    json: true
+  }
+
+  console.log('Initializing Optimizely Client');
+
+  // Options to get the datafile via REST api
+  // let options = {
+  //     uri: `https://www.optimizelyapis.com/experiment/v1/projects/${projectID}/json`,
+  //   headers: {
+  //       'Authorization': 'Bearer ' + process.env.OPTIMIZELY_TOKEN
+  //   },
+  //   json: true
+  // };
+
+  // Returns promise to make GET request to Optimizely API
+  return rp(options)
+    .then((datafile) => {
+      console.log('Here\s the datafile: ', datafile);
+      optimizelyClientInstance = optimizelySDK.createInstance(
+        {
+          datafile: datafile, // Datafile returned from request
+          logger: defaultLogger.createLogger({ logLevel: 1 }) // Default logger provided by SDK
+        }
+      )
+    })
+    .catch((err) => {
+      console.log('There was an error: ', err);
+    })
+  
+};
+
+const OptimizelyDemoHandler = async () => {
+  let userId = '1234sdlkfjdslfkjdsflk5';
+  const html1 = '<h1>Test 1</h1>',
+    html2 = '<h1>Test 2</h2>',
+    html3 = '<h1>Test 3</h2>';
+
+
+  if (!optimizelyClientInstance) {
+    await initOptimizely('17147660659');
+  }
+  var variation = optimizelyClientInstance.activate('Lambda', userId);
+  if (variation === 'variation_1') {
+    // execute code for variation_1
+    optimizelyClientInstance.track('viewPage1', userId);
+    return html1;
+  } else if (variation === 'variation_2') {
+    // execute code for variation_2
+    optimizelyClientInstance.track('viewPage2', userId);
+    return html2;
+  } else {
+    // execute default code
+    return html3;
+  }
+
+
+}
+exports.handler = function (event, context, callback) {
+  console.log(OptimizelyDemoHandler(), 'Op body');
+  OptimizelyDemoHandler().then(function(data){
+    console.log(data, 'data');
+    callback(null, {
+      statusCode: 200,
+      body: data
+    });
   });
 };
